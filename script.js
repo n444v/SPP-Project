1c5cb894f64ed8e46e51c2ede89561d5e04a1960
n444v <122508317+n444v@users.noreply.github.com>
2023-07-31 15:51:27 +0900

    JS CSS HTML

---
+++ script.js
@@ -0,0 +1,33 @@
+// // const animationStats = document.querySelectorAll('.stats-bar');
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('.stats-animate');
+//         };
+//     });
+// });
+// // } else {
+// //     entry.target.classList.remove('.stats-animate');
+// // }};
+// // }
+// // threshold: 0.5
+// //     };
+
+// for (let i = 0; i < animationStats.length; i++) {
+//     const el = animationStats[i];
+
+//     observer.observe(el);
+// }
+
+
+// const observer = new IntersectionObserver(entries => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add('square-animation');
+//         }
+//     });
+// });
+
+
+// observer.observer(document.querySelectorAll)('.stats-bar');
